cb98506bf7cf3e5aabdadc8536129272dfdee54b
MasterLuiji <master199037@gmail.com>
2019-07-07 23:26:29 +0300

    add scrolltotop

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import { MainPage } from "./components/MainPage/MainPage.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Restaurant } from "./components/Restaurant-page/Restaurant.jsx";
+import ScrollToTop from "./ScrollToTop/ScrollToTop.jsx";
 
 function App() {
   return (
     <>
       <Router>
-        <Header />
-        <Route path="/" exact component={MainPage} />
-        <Route path="/Restaurant" component={Restaurant} />
-        <Footer />
+        <ScrollToTop>
+          <Header />
+          <Route path="/" exact component={MainPage} />
+          <Route path="/Restaurant" component={Restaurant} />
+          <Footer />
+        </ScrollToTop>
       </Router>
     </>
   );
