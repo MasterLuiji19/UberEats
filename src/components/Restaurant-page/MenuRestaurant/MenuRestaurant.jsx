94f90e9df69978e41b1dff33319ad87f14cfe062
MasterLuiji <master199037@gmail.com>
2019-07-13 00:17:32 +0300

    start change menu card

--- src/components/Restaurant-page/MenuRestaurant/MenuRestaurant.jsx
+++ src/components/Restaurant-page/MenuRestaurant/MenuRestaurant.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './MenuRestaurant.css'
+import {restaurant} from '../infoCard.js';
 import dish from './img/dish.png'
 
-export function MenuRestaurant() {
+export function MenuRestaurant(props) {
     return(
         <li>
             <a href="javascript:void(0)" className="menu-restaurant">
                 <div className="menu-restaurant__left">
-                    <span className="menu-restaurant__title">Сельдь на бородинском хлебе</span>
+                    <span className="menu-restaurant__title">
+                        {restaurant.items[props.id]}</span>
                     <span className="menu-restaurant__consist">С яйцом и огурцом</span>
                     <span className="menu-restaurant__price">240 &#8372;</span>
                 </div>
