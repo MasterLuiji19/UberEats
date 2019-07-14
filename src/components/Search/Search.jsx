12671891332879100b4dd23fc906a83d10cee040
MasterLuiji <master199037@gmail.com>
2019-07-15 00:26:21 +0300

    change input icon

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,7 +5,6 @@ import search__img from './img/search.svg';
 export function Search() {
     return (
         <section className="search">
-            <img className="search__img" src={search__img} alt="search"/>
             <input className="search__input" type="search" placeholder="Search for restaurant or cuisine"/>
         </section>
     );
