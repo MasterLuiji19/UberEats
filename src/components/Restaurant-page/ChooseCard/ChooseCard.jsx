b47ea03c05b7ce3b1a5426c733537aafd23016f6
MasterLuiji <master199037@gmail.com>
2019-07-14 23:57:26 +0300

    change MenuRestaurant #9

--- src/components/Restaurant-page/ChooseCard/ChooseCard.jsx
+++ src/components/Restaurant-page/ChooseCard/ChooseCard.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import './ChooseCard.css';
 import {MenuRestaurant} from "../MenuRestaurant/MenuRestaurant";
+import {restaurant} from '../infoCard.js';
 
-export function ChooseCard() {
+export function ChooseCard(props) {
     return (
-        <>
-            <div className="choose__title">Закуски</div>
-            <ul className="choose__first-line">
-                <MenuRestaurant/>
-                <MenuRestaurant/>
-                <MenuRestaurant/>
-                <MenuRestaurant/>
-                <MenuRestaurant/>
-                <MenuRestaurant/>
-            </ul>
-        </>
+        <div className="choose-card">
+            {restaurant.sections.map(section => {
+                return (
+                    <div className="choose-type">
+                        <h2 className="choose-title" id={`${section.title}`}>
+                            {section.title}
+                        </h2>
+                        <div key={section.title}>
+                            <div className="choose__first-line">
+                                {section.itemUuids.map(itemUuid => {
+                                    return (
+                                        <MenuRestaurant
+                                            key={itemUuid}
+                                            id={itemUuid}
+                                            order={props.order}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
--- src/components/Restaurant-page/MenuRestaurant/MenuRestaurant.jsx
+++ src/components/Restaurant-page/MenuRestaurant/MenuRestaurant.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import './MenuRestaurant.css'
 import {restaurant} from '../infoCard.js';
-import dish from './img/dish.png'
+import dishCard from './img/dish-card.png'
 
 export function MenuRestaurant(props) {
+    let dishPhoto = restaurant.items[props.id].imageUrl;
     return(
         <li>
             <a href="javascript:void(0)" className="menu-restaurant">
                 <div className="menu-restaurant__left">
                     <span className="menu-restaurant__title">
-                        {restaurant.items[props.id]}</span>
-                    <span className="menu-restaurant__consist">С яйцом и огурцом</span>
-                    <span className="menu-restaurant__price">240 &#8372;</span>
+                        {restaurant.items[props.id].title}
+                    </span>
+                    <span className="menu-restaurant__consist">{restaurant.items[props.id].itemDescription}</span>
+                    <span className="menu-restaurant__price">{restaurant.items[props.id].price/100} &#8372;</span>
                 </div>
-                <img src={dish} alt=""/>
+                {dishPhoto ? (
+                    <img className="menu-restaurant__image" src={restaurant.items[props.id].imageUrl} alt=""/>)
+                    : <img className="menu-restaurant__image" src={dishCard} alt=""/>}
             </a>
         </li>
     );
